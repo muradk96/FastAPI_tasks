9b831a13251ef550be19ec30413f3a28bbb8220e
Murad Kulbuzhev <murad.kulbuzhev@markt-pilot.de>
2024-05-15 22:39:02 +0200

    Add fastapi tutorial12 code

---
+++ tutorial12.py
@@ -0,0 +1,17 @@
+import json
+from enum import Enum
+from typing import Annotated
+from uuid import UUID
+from datetime import datetime, time, timedelta
+
+from fastapi import FastAPI, Query, Path, Body, Cookie, Header
+from pydantic import BaseModel, Field, HttpUrl
+
+app = FastAPI()
+
+## Part 12 - Cookie and Header Parameters
+
+@app.get("/items")
+async def read_items(cookie_id: str | None = Cookie(None), accept_encoding: str | None = Header(None),
+                     sec_ch_ua: str | None = Header(None), user_agent: str | None = Header(None), x_token: list[str] | None = Header(None)):
+    return {"cookie_id": cookie_id, "Accept-Encoding": accept_encoding, "sec-ch-ua": sec_ch_ua, "User-Agent": user_agent, "X-Token values": x_token}
