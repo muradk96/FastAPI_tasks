0762ea86e047391df6ef752ac3d739d1180f95fb
Murad Kulbuzhev <murad.kulbuzhev@markt-pilot.de>
2024-05-20 20:02:33 +0200

    Add tutorial7.py

---
+++ tutorial7.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, Query, Path, Body
+from pydantic import BaseModel
+from enum import Enum
+
+from typing import List, Optional
+
+app = FastAPI()
+
+## Part 7: Body - Multiple Parameters
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+            *,
+            item_id: int = Path(..., title = "The ID of the item to get", ge=0, le=150),
+            q: str | None = None,
+            item: Item = Body(..., embed=True),
+            # user: User,
+            # importance: int = Body(...)
+            ):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q":q})
+
+    if item:
+        results.update({"item": item})
+
+    # if user:
+    #     results.update({"user": user})
+    #
+    # if importance:
+    #     results.update({"importance": importance})
+
+    return results
